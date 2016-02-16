4dfa8717bbf3bb0c0152e6518279c525c45190b0
biarca-nareshtr <naresht@biarca.com>
2016-02-16 01:40:20 -0800

    updated comments

--- OpenVPN/package/Resources/scripts/deployOpenVPNClient.sh
+++ OpenVPN/package/Resources/scripts/deployOpenVPNClient.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
+# This will invoke client.sh which generates client.crt client.key files in server instance and copy to clients such as GCE 
 bash /opt/openvpn/client.sh >> /tmp/openvpn-client.log
-
